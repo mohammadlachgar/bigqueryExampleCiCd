553f63e238af8596c7620a2a8a5ac20aa7cbd056
Mohammad <mohammad4cj@gmail.com>
2020-08-13 21:09:01 +0100

    a

--- cmd.sh
+++ cmd.sh
@@ -13,4 +13,4 @@ touch ./union.sql
 
 #merge all content in 'hql_run' and 'sql_run' to one file "union.sql"
 find hql_run/. -type f -name "*.hql" | xargs cat > ./union.sql
-find sql_run/. -type f -name "*.sql" | xargs cat > ./union.sql
+find sql_run/. -type f -name "*.sql" | xargs cat >> ./union.sql
