b94f07c66699c48ed4cd1364139b75ef827ed94d
Mohammad <mohammad4cj@gmail.com>
2020-08-13 12:45:59 +0100

    try create table

---
+++ my_query.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `burnished-case-280710.ex_dataset1.ST1`;
+CREATE TABLE `burnished-case-280710.ex_dataset1.ST1`
+(
+    `ADDRESS_ID` STRING,
+     `INDIVIDUAL_ID` STRING,
+      `FIRST_NAME` STRING,
+       `LAST_NAME` STRING
+);
