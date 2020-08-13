7da00466ee617c4dc0604509a8971fa69ce94e7c
Mohammad <mohammad4cj@gmail.com>
2020-08-13 20:50:23 +0100

    change comment

--- cmd.sh
+++ cmd.sh
@@ -9,6 +9,6 @@ s/CREATE\s+EXTERNAL\s+TABLE/CREATE TABLE/g;
 s/(\n|\r)\s*\)\s*(\n|\r).*(\n*\r*\n*.*)*/\n);/g
 '
 
-#merge all content to one file
+#merge all content in 'hql_run' and 'sql_run' to one file "union.sql"
 find hql_run/. -type f -name "*.hql" && find sql_run/. -type f -name "*.sql" | xargs cat > ./union.sql
 
