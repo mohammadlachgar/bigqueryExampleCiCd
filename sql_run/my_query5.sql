51467968ef292a0ec9ec78113712297ef4918367
Mohammad <mohammad4cj@gmail.com>
2020-08-13 21:19:03 +0100

    a

---
+++ sql_run/my_query5.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `burnished-case-280710.dataset_ex2.ST5`;
+CREATE TABLE `burnished-case-280710.dataset_ex2.ST5`
+(
+    `ADDRESS_ID5555` STRING,
+     `INDIVIDUAL_ID` STRING,
+      `FIRST_NAME` STRING,
+       `LAST_NAME` STRING
+);
