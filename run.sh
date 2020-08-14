79c4267e520f0e273eebf9df7e8ff13085b18e93
Mohammad <mohammad4cj@gmail.com>
2020-08-14 13:51:22 +0100

    try add variables to run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+projectId="burnished-case-280710"
+location_bigquery="US"
+name_secret="SERVICEACCOUNT_bigquery"
 
-mkdir -p ./creds    
-gcloud secrets versions access latest --secret=SERVICEACCOUNT_bigquery --format='get(payload.data)' | tr '_-' '/+' | base64 -d > ./creds/serviceaccount.json
+mkdir -p ./creds 
+
+# Add secret "SERVICEACCOUNT_bigquery" to /security/secret-manager
+# Enable "Secret Manager" in cloud-build/settings
+gcloud secrets versions access latest --secret=$name_secret --format='get(payload.data)' | tr '_-' '/+' | base64 -d > ./creds/serviceaccount.json
+
 gcloud auth activate-service-account --key-file ./creds/serviceaccount.json
+
 chmod +x ./cmd.sh  && ./cmd.sh
-bq --project_id=burnished-case-280710 --location=US query --nouse_legacy_sql < union.sql
+
+bq --project_id=$projectId --location=$location_bigquery query --nouse_legacy_sql < union.sql
 
 
