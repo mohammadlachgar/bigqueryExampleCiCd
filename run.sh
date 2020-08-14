fe18d2e5472545d088772b7986c95c997f976a72
Mohammad <mohammad4cj@gmail.com>
2020-08-14 14:12:52 +0100

    try import variable file

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-projectId="burnished-case-280710"
-location_bigquery="US"
-name_secret="SERVICEACCOUNT_bigquery"
-dataset_bigquery="dataset_ex2"
+
+. ./variables.sh
 
 mkdir -p ./creds 
 
