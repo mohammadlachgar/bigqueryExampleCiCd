891b0d7f21d3226551bd8ee723116a456359d87b
Mohammad <mohammad4cj@gmail.com>
2020-08-14 12:13:21 +0100

    fix

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ mkdir -p ./creds
 gcloud secrets versions access latest --secret=SERVICEACCOUNT_bigquery --format='get(payload.data)' | tr '_-' '/+' | base64 -d > ./creds/serviceaccount.json
 gcloud auth activate-service-account --key-file ./creds/serviceaccount.json
 chmod +x ./cmd.sh  && ./cmd.sh
-bq --project_id=$PROJECT_ID --location=US query --nouse_legacy_sql < union.sql
+bq --project_id=burnished-case-280710 --location=US query --nouse_legacy_sql < union.sql
 
 
