d2f7e82338e73c63ffe2f65414ac3e7b3277fee1
Mohammad <mohammad4cj@gmail.com>
2020-09-07 16:47:11 +0100

    test run bigquery from other project

--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-projectId="burnished-case-280710"
+projectId="winter-agility-288815"
 location_bigquery="US"
-name_secret="SERVICEACCOUNT_bigquery"
+name_secret="SERVICEACCOUNT_bigquery_from_project58412"
 dataset_bigquery="dataset_ex2"
