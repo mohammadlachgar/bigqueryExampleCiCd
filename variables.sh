6e81380d4af862a0279459ede607844aa1fabbb3
Mohammad <mohammad4cj@gmail.com>
2020-09-08 12:43:05 +0100

    try use zetasql

--- variables.sh
+++ variables.sh
@@ -4,3 +4,9 @@ projectId="winter-agility-288815"
 location_bigquery="US"
 name_secret="SERVICEACCOUNT_bigquery_from_project58412"
 dataset_bigquery="dataset_ex2"
+
+
+projectId="winter-agility-288815"
+location_bigquery="US"
+name_secret="SERVICEACCOUNT_bigquery_from_project58412"
+dataset_bigquery="dataset_ex2"
